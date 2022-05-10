d0eadf7b62ef5d491208e9bd577ce71909d1f371
Hugo <a21hugni@student.his.se>
2022-05-11 00:51:37 +0200

    Lade till alla variabler, satte onClick på tillbaka knappen och gjorde så att en editor skriver det skrivna namnet

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,13 +2,44 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button tillbaka;
+    EditText namn;
+    SharedPreferences preferences;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        namn = findViewById(R.id.edit_text);
+        button = findViewById(R.id.save);
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        tillbaka = findViewById(R.id.tillback);
+        tillbaka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("name", namn.getText().toString());
+                editor.apply();
+            }
+        });
     }
 }
