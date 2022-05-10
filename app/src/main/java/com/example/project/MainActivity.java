1d156ae9b29ca6d84b69cd70c0e67cd9f81453f3
Hugo <a21hugni@student.his.se>
2022-05-11 00:26:00 +0200

    Skapade en till activity med tillhörande layout

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +12,6 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView text;
-    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        text = findViewById(R.id.name);
 
-        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("name", "Huggenator");
-        editor.apply();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        String name = preferences.getString("name", "inget namn hittades");
-        text.setText(name);
-    }
 }
